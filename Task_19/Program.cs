3f4be1a5f26bc7e3944400d085b992f6a8e36883
vivimila <violamilashevskaya@yandex.by>
2022-08-17 17:35:06 +0300

    Create a folder task 21

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -4,30 +4,30 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int CheckNumber(int num)
+// int CheckNumber(int num)
 
-{
-    int firstNumber = num / 10000; //находим первую цифру
-    int lastNumber = num % 10000; //находим последнюю цифру
-    int secondNumber = firstNumber % 10; //находим вторую цифру
-    int foursNumber = num / 10; //отбрасываем последнюю цифру
-    int nov = foursNumber % 100;
-    return nov;
+// {
+//     int firstNumber = num / 10000; //находим первую цифру
+//     int lastNumber = num % 10000; //находим последнюю цифру
+//     int secondNumber = firstNumber % 10; //находим вторую цифру
+//     int foursNumber = num / 10; //отбрасываем последнюю цифру
+//     int nov = foursNumber % 100;
+//     return nov;
    
-    if (firstNumber == lastNumber && secondNumber == nov) 
-{
-  Console.WriteLine($"Пятизначное число {number} является палиндромом!");
-}
-  else if (firstNumber != lastNumber || secondNumber != nov)
-   {
-    Console.WriteLine($"Пятизначное число {number} является палиндромом!");
-  }
+//     if (firstNumber == lastNumber && secondNumber == nov) 
+// {
+//   Console.WriteLine($"Пятизначное число {number} является палиндромом!");
+// }
+//   else if (firstNumber != lastNumber || secondNumber != nov)
+//    {
+//     Console.WriteLine($"Пятизначное число {number} является палиндромом!");
+//   }
 
-int result = CheckNumber(number);
-}
+// int result = CheckNumber(number);
+// }
     
 
 	
